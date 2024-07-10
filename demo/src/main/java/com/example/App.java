bb2697e7c2ab7b45af56b17081e16028f6f66db1
FernanGR <47358813+FernanGR@users.noreply.github.com>
2024-07-10 12:25:56 +0000

    bbdd

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,5 +1,15 @@
 package com.example;
 
+import java.util.UUID;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.example.entity.Foo;
+
 /**
  * Hello world!
  *
@@ -9,5 +19,31 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static void setupHibernate(){
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .build();
+        SessionFactory sessionFactory = new MetadataSources(registry)
+            .addAnnotatedClass(Foo.class)
+            .buildMetadata()
+            .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        var tr = session.beginTransaction();
+        Foo foo = new Foo();
+        foo.id = UUID.randomUUID();
+        foo.name = "jesus";
+        foo.dni="12345A";
+        
+        //Foo foo = session.get(Foo.class,1);
+        //session.remove(Foo.class, 1);
+        // if (foo!= null){
+        //     session.remove(Foo.class,1);    
+        //  }
+        session.persist(foo);
+        tr.commit();
+        session.close();
+
     }
 }
---
+++ demo/src/main/java/com/example/entity/Foo.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="foo")
+public class Foo {
+    @Id
+    public UUID id;
+    public String name;
+    public String dni;
+    
+}
